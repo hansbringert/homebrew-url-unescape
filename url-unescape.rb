80aa1132c89a62936e0a622bbf0156ba508e414b
Hans Bringert <hans.bringert@infomaker.se>
2018-11-30 07:32:44 +0100

    version 0.5

--- url-unescape.rb
+++ url-unescape.rb
@@ -1,8 +1,8 @@
 class UrlUnescape < Formula
   desc "url-unescape  CLI"
   homepage "https://bitbucket.org/infomaker/"
-  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-unescape/url-unescape-0.4.tar.gz"
-  sha256  "4687c4e653a9d142331b1e8c4499e5405bbbb45087aeb299fdf870416876a549"
+  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-unescape/url-unescape-0.5.tar.gz"
+  sha256  "8485a625a5997c8865524ccba785e649885669032d720f382921211ad650974e"
 
   # depends_on "go" => :build
 
